2009bb053b9fbff8943c1cea40f3805c6f819a7f
Roy Wright <roy@wright.org>
2009-04-15 19:02:12 -0500

    initial release

---
+++ lib/object_extensions.rb
@@ -0,0 +1,19 @@
+# == Synopsis
+# add a blank? method to all Objects
+class Object
+  # return asserted if object is nil or empty
+  # TODO: not the safest coding, probably should dup before stripping.  Maybe should also compact
+  def blank?
+    result = nil?
+    unless result
+      if respond_to? 'empty?'
+        if respond_to? 'strip'
+          result = strip.empty?
+        else
+          result = empty?
+        end
+      end
+    end
+    result
+  end
+end
