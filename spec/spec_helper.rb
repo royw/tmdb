c233d8319965f161e6b6b7cfd514ae88b0fb2975
Roy Wright <roy@wright.org>
2009-04-20 02:09:28 -0500

    replaced non-DRY cache_extensions with meta programming

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,13 +2,15 @@ require 'spec'
 
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require 'tmdb'
 
-require 'cache_extensions'
+require 'tmdb'
 
 $samples_dir = File.dirname(__FILE__) + '/samples'
 
 TMPDIR = File.join(File.dirname(__FILE__), '../tmp')
+
+require 'cache_extensions'
+CacheExtensions.attach_to_read_page_classes($samples_dir)
 
 Spec::Runner.configure do |config|
 
